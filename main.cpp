30ca72798d117d0e494deb6d076a2d7a73b5e7ab
DodoRoman <115349003+DodoRoman@users.noreply.github.com>
2022-11-25 15:59:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main() {
    int n=0;
-   cin >> n;
    int a=1;
    int b=1;
    int c=0;
+   cin >> n;
    for (int i=0; i<=n; i++){
        if (a<=n) {
            cout << a << endl;
