5eaba2d0e017c7627e386bc1130169f6d98ba7cc
DodoRoman <115349003+DodoRoman@users.noreply.github.com>
2022-11-25 15:55:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+   int n=0;
+   cin >> n;
+   int a=1;
+   int b=1;
+   int c=0;
+   for (int i=0; i<=n; i++){
+       if (a<=n) {
+           cout << a << endl;
+           c = a + b;
+           a = b;
+           b = c;
+       }
+   }
    return 0;
 }
